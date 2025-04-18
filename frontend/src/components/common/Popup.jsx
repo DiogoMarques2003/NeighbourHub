1d0082ddf592897325e8b793996840e6d199f2a4
Diogo <diogoduarte896@gmail.com>
2025-04-18 16:59:57 +0100

    changes

--- frontend/src/components/common/Popup.jsx
+++ frontend/src/components/common/Popup.jsx
@@ -1,4 +1,6 @@
-const Popup = ({ openPopUp, closePopUp }) => {
+import { X } from 'lucide-react';
+
+const Popup = ({ openPopUp, closePopUp, popupTitle, children, popupHandleSubmit }) => {
 
     const handlelosePopUp = (e) => {
     if (e.target.id === 'ModelContainer') {
@@ -12,15 +14,29 @@ const Popup = ({ openPopUp, closePopUp }) => {
     <div
       id='ModelContainer'
       onClick={handlelosePopUp}
-      className='fixed inset-0 flex justify-center items-center bg-opacity-20 backdrop-blur-sm'>
+      className=' fixed inset-0 flex justify-center items-center bg-opacity-20 backdrop-blur-sm'>
       <div 
-        className='p-2 bg-white w-10/12 md:w-1/2 lg:1/3 shadow-inner border-e-emerald-600 rounded-lg py-5'>
-        <div
-          className='w-full p-3 justify-center items-center'>
+        className='p-2 bg-white w-11/12 md:w-1/3 lg:1/4 shadow-inner border border-gray-200 rounded-lg py-5'>
+        <div className='w-full p-3 justify-center items-center'>
+          <div className='flex justify-center items-center'>
           <h2
-            className='font-semibold py-3 text-center text-xl'>
-              My PopUP
+            className='font-semibold text-center text-xl'>
+              {popupTitle}
           </h2>
+          <div className="justify-self-end">
+            <button type="button" class="text-gray-500 hover:bg-gray-200 focus:outline-none font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center cursor-pointer  "
+            onClick={closePopUp}>
+              <X></X>
+            <span class="sr-only">Icon description</span>
+            </button>
+          </div>
+        </div>
+          <hr class="h-px my-4 bg-gray-300 border-0"></hr>
+          <div className="space-y-4">
+            <form onSubmit={popupHandleSubmit} className="space-y-4">
+              {children}
+            </form>
+          </div>
         </div>
       </div>
     </div>
--- frontend/src/pages/ServicesPage.jsx
+++ frontend/src/pages/ServicesPage.jsx
@@ -1,23 +1,12 @@
-import Button from "../components/common/Button";
 import { useNavigate } from 'react-router-dom';
-import Popup from "../components/common/Popup";
-import { useState } from "react";
+import CreateServiceForm from "../components/features/createService/CreateServiceForm";
 
 const ServicesPage = () => {
     const navigate = useNavigate();
 
-    const [openPopup, setOpenPopup] = useState(false);
-
-    const HandleRemovePopUp = () => setOpenPopup(false);
-
     return(
         <>
-        <Button
-        onClick={() => setOpenPopup(true)}
-        >
-            Criar novo  serviço
-        </Button>
-        <Popup openPopUp={openPopup} closePopUp={HandleRemovePopUp} />
+         <CreateServiceForm/>
         </>
     )
 }
