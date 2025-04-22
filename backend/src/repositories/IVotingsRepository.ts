12122c48fbb0035f81b6b24135211979b212fd65
Diogo Marques <diogomarques2003@protonmail.com>
2025-04-22 19:28:45 +0100

    Lógica de contar os votos finalizada

--- backend/src/repositories/IVotingsRepository.ts
+++ backend/src/repositories/IVotingsRepository.ts
@@ -3,5 +3,5 @@ import Votings from '@entities/Votings';
 export default interface IVotingsRepository {
   findByOrderAndUser(orderID: string, userID: string): Promise<Votings | null>;
   upsert(voting: Votings): Promise<Votings>;
-  countByOrder(orderId: string): Promise<{ upVotes: number; downVotes: number }>;
+  countByOrder(orderId: string, condominiumId: string): Promise<{ upVotes: number; downVotes: number }>
 }
